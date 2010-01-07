6d638bd7817faf6fc81c30b8e156c36a22beaf74
Joel Friedman <asher.friedman@gmail.com>
2010-01-06 22:20:45 -0600

    attempting to uninstall correctly

--- uninstall.rb
+++ uninstall.rb
@@ -1,1 +1,26 @@
 # Uninstall hook code here
+require 'fileutils'
+
+PUBLIC = File.join Rails.root, "public"
+CSS = File.join PUBLIC, "stylesheets"
+JS = File.join PUBLIC, "javascripts", "jquery.fancy_box"
+IMGS = File.join PUBLIC, "images"
+FANCY_BOX_PATH = File.join Rails.root, "vendor", "plugins", "fancy_box_rails_plugin"
+
+# Install css
+css_files = Dir.glob(File.join(CSS, "css", "*fancybox.css"))
+FileUtils.rm css_files
+
+# Install javascript
+FileUtils.rm_rf JS
+#Dir.mkdir JS unless File.exists?(JS)
+#js_files = Dir.glob(File.join(FANCY_BOX_PATH,"js","*"))
+#FileUtils.cp_r js_files, JS
+
+# Install images
+public_path = File.join(IMGS,"fancy_box")
+FileUtils.rm_rf public_path
+
+#Dir.mkdir public_path unless File.exists?(public_path)
+#imgs = Dir.glob(File.join(FANCY_BOX_PATH,"images","*.{png,gif}"))
+#FileUtils.cp_r imgs, public_path
